458795c79077e6a5611eb14ac703616dbde4e9c6
porterrakisits <prakisits@byupathway.edu>
2025-01-30 21:33:35 -0700

    beginging of project

---
+++ Project/scripts/siteplan.js
@@ -0,0 +1,18 @@
+// The display of current year and date of last modification in the footer
+function displayCurrentYear() {
+    const currentYear = new Date().getFullYear(); 
+    const yearSpan = document.getElementById('current-year'); 
+    yearSpan.textContent = currentYear; 
+}
+
+function displayLastModifiedDate() {
+    const lastModifiedDate = new Date(document.lastModified); 
+    const formattedDate = lastModifiedDate.toLocaleDateString();
+    const modifiedParagraph = document.getElementById('lastModified'); 
+    modifiedParagraph.textContent = `Last modified on: ${formattedDate}`; 
+}
+
+displayCurrentYear();
+displayLastModifiedDate();
+
+
